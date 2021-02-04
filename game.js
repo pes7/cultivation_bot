f6ad7656d6bd940ec7dd26f1dfedd41157f17148
Назар <wowpes7@mail.ru>
2021-02-04 17:51:24 +0200

    v 0.0.4 with bug ;)

--- game.js
+++ game.js
@@ -14,7 +14,8 @@ bot.use(arGs());
 const _DB = "Bot";
 const _tables = {
   _usersTable:"Users",
-  _sectTable:"Sects"
+  _sectTable:"Sects",
+  _wildTable:"Wild"
 };
 const _setting = { useUnifiedTopology: true,connectTimeoutMS: 30000,keepAlive: 1 };
 const _url = 'mongodb://root:password@localhost:27017/';
@@ -23,46 +24,16 @@ const _url = 'mongodb://root:password@localhost:27017/';
 const _helpCommands = 
 `/cultivate - Культивировать сколько секунд.
 /immortals - Покажет топ культиваторов и их уровень.
+/clCancle - Отменить культивацию! (Очень опасно, вы можете потерять свой прогресс культивации).
 `
-
-/*
-class Wild{
-  Wild = {
-    "name":'',
-    "desc":'',
-    "type":'',
-    "price":0,
-    "cultivate":0,
-    "stat":{
-        "attak":3,
-        "defend":1,
-        "dodge":1
-    }
-  }
-  constructor(jn) {
-    this.Wild.name = js.name;
-    this.Wild.desc = js.desc;
-    this.Wild.type = js.type;
-    this.Wild.price = js.price;
-    this.Wild.cultivate = js.cultivate;
-    this.Wild.stat.attak = js.stat.attak;
-    this.Wild.stat.defend = js.stat.defend;
-    this.Wild.stat.dodge = js.stat.dodge;
-  }
-}
-*/
-
-/*Load JSON*/
-//const ww = JSON.parse(fs.readFileSync('json/wild.json', 'utf8'));
-/*Check JSON read*/
-//console.log(Wild);
 
 class User {
   static NAMES;
   Character = {
     Info:{_id:0,_name:''},
     Cultivation:{_level:0,_points:0},
-    TimeOut:{_cultivate:0}
+    TimeOut:{_cultivate:0},
+    Interval:{_cultivate_update:0}
   }
   constructor(from) {
     this.Character.Info._id = from.id;
@@ -76,16 +47,35 @@ class dbWork {
     dbWork.creatTable(table);
     var table = _tables._sectTable;
     dbWork.creatTable(table);
+    var table = _tables._wildTable;
+    dbWork.creatTable(table, () => {
+      /*Load JSON*/
+      const ww = JSON.parse(fs.readFileSync('json/wild.json', 'utf8'));
+      var ii = Object.keys(ww).map(key => {
+        return ww[key];
+      })
+      const mongoClient = new MongoClient(_url, _setting);
+      mongoClient.connect(function(err, client){
+        const db = client.db(_DB);
+        const collection = db.collection(_tables._wildTable);
+        collection.insertMany(ii, function(err, result){
+            if(err){ 
+                return console.log(err);
+            }
+            client.close();
+        });
+      });
+    });
   }
 
-  static creatTable(table){
+  static creatTable(table, clb = undefined){
     console.log(`Check ${table}`)
     const client = new MongoClient(_url,_setting);
     client.connect(function(err) {
       if (err) { console.log(`Database ${_DB} not EXIST!!! Create IT NOW!!!!`); return false; };
       var db = client.db(_DB);
         db.createCollection(table, function(err, res) {
-          if (err.code != 48) console.log(err);
+          if (err?.code != 48) {console.log(err); clb ? clb(): console.log(`${table} no clb`); }
           console.log(`Collection ${table} created!`);
           client.close();
         });
@@ -162,30 +152,53 @@ bot.command('cultivate', (ctx) => {
       ctx.reply('Я уже культивирую! Хочешь отменить? \n/clCancle');
       return false;
     } 
-    //Начинаем
-    ctx.reply('Культивирую!');
     //Аргументы
     var time_arg = ctx.state.command.args[0];
     console.log(`id: ${ctx.update.message.from?.id} name: ${ctx.update.message.from?.first_name} time: ${time_arg}`);
     var time = parseInt(time_arg);
     time = time * 1000;
 
-    //По окончанию
-    function endCultivate(arg) {
-      dbWork.getUser(ctx.update.message.from,(us)=>{
-        arg.reply(`Культивация окончена!`);
-        us.Character.Cultivation._points = us.Character.Cultivation._points + time/1000;
-        us.Character.TimeOut._cultivate = 0; //Чистим таймаут!
-        dbWork.updateUser(us, (tr)=>{ console.log(`cultivate update ${us.Character.Info._name}: ${tr}`) });
-        console.log(`Name: ${us.Character.Info._name}, score: ${us.Character.Cultivation._points}`)
-      })
-    }
-    //Сначало это. Моментное действие, user не перечитем.
-    let timeout = setTimeout(endCultivate, time, ctx);
-    user.Character.TimeOut._cultivate = parseInt(timeout);
-    dbWork.updateUser(user);
+    //Начинаем
+    var s = ctx.reply(`Культивирую: ${time_arg}`);
+    s.then((x)=>{
+      var m_id = x.message_id;
+      var c_id = x.chat.id;
+      var _iterv = setInterval((m,c)=>{
+        ctx.telegram.editMessageText(c,m,m`Культивирую: ${time_arg - _time_pass}`);
+      },1000,m_id,c_id)
+      user.Character.Interval._cultivate_update = _iterv;
+      dbWork.updateUser(user, (user_in) => {
+        //По окончанию
+        function endCultivate(arg) {
+          dbWork.getUser(ctx.update.message.from,(us)=>{
+            arg.reply(`Культивация окончена!`);
+            us.Character.Cultivation._points = us.Character.Cultivation._points + time/1000;
+            us.Character.TimeOut._cultivate = 0; //Чистим таймаут!
+            dbWork.updateUser(us, (tr)=>{ console.log(`cultivate update ${us.Character.Info._name}: ${tr}`) });
+            console.log(`Name: ${us.Character.Info._name}, score: ${us.Character.Cultivation._points}`)
+          })
+        }
+        //Сначало это. Моментное действие, user не перечитем.
+        let timeout = setTimeout(endCultivate, time, ctx);
+        user_in.Character.TimeOut._cultivate = parseInt(timeout);
+        dbWork.updateUser(user_in);
+      });
+    })
   })
-  
+})
+
+bot.command('clCancle', (ctx) => {
+  dbWork.getUser(ctx.update.message.from, (user) => {
+    if(user != undefined){
+      if(user.Character.TimeOut._cultivate != 0){
+        clearTimeout(user.Character.TimeOut._cultivate);
+        clearInterval(user.Character.Interval._cultivate_update);
+        user.Character.TimeOut._cultivate = 0;
+        user.Character.Interval._cultivate_update = 0;
+        dbWork.updateUser(user, (tr)=>{ ctx.reply('Культивация прервана - прогресс утерян...'); console.log(`cultivate cancle ${user.Character.Info._name}: ${tr}`) });
+      }else{ctx.reply('Что отменять то? Вы и так не культивируете, лодрь!');}
+    }
+  })
 })
 
 bot.command('immortals', (ctx) => {
