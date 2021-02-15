36b55af3b5ac1b5b53c44c3c67f6b7cc852b841c
Назар <wowpes7@mail.ru>
2021-02-15 17:42:57 +0200

    upd v0.1.1 Battle, Battle End, Items, Inventory

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
 const schedule = require('node-schedule');
-const { Telegraf } = require('telegraf')
+const { Markup,Telegraf } = require('telegraf')
 const MongoClient = require("mongodb").MongoClient;
 const fs = require('fs');
 const { log } = require('console');
@@ -7,7 +7,7 @@ const { brotliCompress } = require('zlib');
 const { ifError, throws } = require('assert');
 const arGs = require('./commandArgs');
 
-const _DEBUG = false;
+const _DEBUG = true;
 
 const token = "1063342985:AAHvN1QdZUt90BAsdo-Qc3as7pGz-HaspNA";
 const bot = new Telegraf(token)
@@ -20,8 +20,9 @@ const _tables = {
   _usersTable:"Users",
   _sectTable:"Sects",
   _wildTable:"Wild",
-  _location:"Location",
-  _battle:"Battle"
+  _locationTable:"Location",
+  _battleTable:"Battle",
+  _itemTable:"Items"
 };
 const _setting = { useUnifiedTopology: true,connectTimeoutMS: 30000,keepAlive: 1 };
 const _url = 'mongodb://root:password@localhost:27017/';
@@ -65,10 +66,11 @@ class User {
   static NAMES;
   Character = {
     Info:{_id:0,_name:'',_loc:'d_ShanSci'},
-    Stat:{_hp:100,_attak:6,_defend:2,_dodge:1},
+    Stat:{_hp:100,_attak:6,_defend:2,_dodge:1,_realHp:100},
     Cultivation:{_level:0,_points:0},
     TimeOut:{_cultivate:0},
-    Interval:{_cultivate_update:0}
+    Interval:{_cultivate_update:0},
+    Inventory:[]
   }
   constructor(from) {
     this.Character.Info._id = from?.id;
@@ -90,6 +92,34 @@ class User {
         ctx.reply('Немогу найти путь к этому месту...');
        }
       }
+    })
+  }
+  addItemRange(items){
+    items.forEach(it => {
+      if(Object.keys(this.Character.Inventory).length > 0){
+        var found = this.Character.Inventory.find(element => element.n==it.n);
+        if(found != undefined) {
+          found.count = found.count + it.count;
+        }else{ this.Character.Inventory.push({n:it.n,count:it.count}); }
+      }else{
+        this.Character.Inventory.push({n:it.n,count:it.count});
+      }
+    });
+    User.updateUser(this,(tr)=>{ // Треба тестить abo if(ii >= Object.keys(loc.wild).length) {
+      if(_DEBUG === true){console.log(`${this.Character.Info._name} add items: ${items}`)}
+    })
+  }
+  addItem(item,count){
+    if(Object.keys(this.Character.Inventory).length > 0){
+      var found = this.Character.Inventory.find(element => element.n==item);
+      if(found != undefined) {
+        found.count = found.count + count;
+      }else{ this.Character.Inventory.push({n:item,count:count}); }
+    }else{
+      this.Character.Inventory.push({n:item,count:count});
+    }
+    User.updateUser(this,(tr)=>{
+      if(_DEBUG === true){console.log(`${this.Character.Info._name} add item: ${item} count: ${count}`)}
     })
   }
   static insertUser(user) {
@@ -153,7 +183,7 @@ const _defender_types = {
   _bot:"Bot"
 }
 class Battle{
-  static table = _tables._battle;
+  static table = _tables._battleTable;
   Battle = {
     _attaker:'',
     _defender:'',
@@ -175,10 +205,10 @@ class Battle{
         const collection = db.collection(Battle.table);
         var battle = new Battle();
         battle.Battle._attaker = user.Character.Info._id;
-        battle.Battle._attaker_hp = user.Character.Stat._hp;
+        battle.Battle._attaker_hp = user.Character.Stat._realHp;
         battle.Battle._defender_type = _defender_types._player;
         battle.Battle._defender = wild.n;
-        battle.Battle._defender_hp = wild.stat._hp;
+        battle.Battle._defender_hp = wild.stat.hp;
         battle.Battle._attaker_timer = 60;
         battle.Battle._defender_timer = 60;
         battle.Battle._timeStamp = Date.now();
@@ -230,7 +260,7 @@ class Battle{
       });
     })
   }
-  static deleteBattle(battle, clb){
+  static deleteBattle(battle, clb = (tr)=>{if(_DEBUG) console.log(`Delete ${tr}`)}){
     const mongoClient = new MongoClient(_url, _setting);
     mongoClient.connect(function(err, client){
       if (err) console.log(err,'err');
@@ -240,28 +270,119 @@ class Battle{
       collection.deleteOne(myquery, function(err, obj) {
         if (err) console.log(err,'err');
         console.log(`Battle ${battle.Battle._attaker} deleted!`);
-        clb();
+        clb(true);
         client.close();
       });
     })
   }
-  //NEED DELET Function, after end of battle
+
   static battle(user,ctx){
     Battle.getBattleByUser(user, (battle)=>{
-      var s = ctx.reply(`Противник`);
-      s.then((x)=>{
-        battle.Battle.message._id = x.message_id;
-        battle.Battle.message._chatId = x.chat.id;
-        Battle.updateBattle(battle,()=>{ //Теперь у нас есть линканутое сообщение
-          console.log('Пока что всё') //ДОДЕЛАТЬ!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+      Wild.getWildByName(battle.Battle._defender,(enemy)=>{
+        var s = ctx.reply(`Противник ${enemy.name}\nЗдоровья: ${battle.Battle._defender_hp}/${enemy.stat.hp}\nВаше здоровье: ${battle.Battle._attaker_hp}/${user.Character.Stat._hp}`);
+        s.then((x)=>{
+          battle.Battle.message._id = x.message_id;
+          battle.Battle.message._chatId = x.chat.id;
+          Battle.updateBattle(battle,()=>{ //Теперь у нас есть линканутое сообщение
+            var inlineMessageRatingKeyboard = Markup.inlineKeyboard([
+              Markup.button.callback('⚔️', 'battle_attak'),
+              Markup.button.callback('🔙', 'battle_retreat')
+            ])
+            ctx.telegram.sendMessage(
+                ctx.from.id,
+                'Действия:',
+                inlineMessageRatingKeyboard)
+          })
         })
       })
-    })
+    });
+  }
+}
+
+bot.on('callback_query', (ctx) => {
+  // Explicit usage
+  ctx.telegram.answerCbQuery(ctx.callbackQuery.id)
+  if(_DEBUG === true) {console.log(`${ctx.from.id} ${ctx.from.username} action ${ctx.callbackQuery.data}`)}
+  User.getUser(ctx.from,(user)=>{
+    if(user != undefined){
+      Battle.getBattleByUser(user,(battle)=>{
+        if(battle != undefined){
+          if(ctx.callbackQuery.data == 'battle_attak'){
+            Wild.getWildByName(battle.Battle._defender,(enemy)=>{
+              if(enemy != undefined){
+                battle.Battle._defender_hp = battle.Battle._defender_hp - user.Character.Stat._attak; //ДОБАВИТЬ ЗАЩИТУ И УВОРОТ!!!!
+                battle.Battle._attaker_hp = battle.Battle._attaker_hp - enemy.stat.attak;
+
+                if(battle.Battle._defender_hp <= 0){ //ПОБЕДА, нужно добавить выпадения лута
+                  user.Character.Stat._realHp = battle.Battle._attaker_hp;
+                  user.Character.Cultivation._points = user.Character.Cultivation._points + enemy.cultivate;
+                  User.updateUser(user,()=>{
+                    var us = Object.assign(new User, user);
+            
+                    ctx.telegram.editMessageText(battle.Battle.message._chatId,battle.Battle.message._id,battle.Battle.message._id,
+                      `Победа!\nВаше здоровье: ${battle.Battle._attaker_hp}/${user.Character.Stat._hp}\nКультивация +${enemy.cultivate}\nКультивация сейчас:${user.Character.Cultivation._points}`);
+
+                    ctx.deleteMessage(ctx.callbackQuery.message.message_id);
+                    Battle.deleteBattle(battle);
+
+                    var items = [];
+                    for (var l of enemy.loot) {
+                      if(getRandomArbitrary(0,1000) <= l.chance)
+                      {
+                        if(items.length == 0) ctx.reply('Вы получили:')
+                        var co = Math.floor(getRandomArbitrary(l.c_min,l.c_max));
+                        items.push({n:l.n,count:co})
+                        Item.getItemByName(l.n,(item)=>{ctx.reply(`${item.name} - ${co}`)})
+                      }
+                    }
+                    console.log(items);
+                    us.addItemRange(items);
+                    return true;
+                  })
+                }else if(battle.Battle._attaker_hp <= 0){//Поражение -----------------------------------------------------------------------ДОДЕЛАТЬ!!!!
+                  ctx.reply('Этого пока нет)');
+                  battleLeave(ctx);
+                }else{
+                  Battle.updateBattle(battle,()=>{
+                    ctx.telegram.editMessageText(battle.Battle.message._chatId,battle.Battle.message._id,battle.Battle.message._id,
+                      `Противник ${enemy.name}\nЗдоровья: ${battle.Battle._defender_hp}/${enemy.stat.hp}\nВаше здоровье: ${battle.Battle._attaker_hp}/${user.Character.Stat._hp}`);
+                  });
+                }
+             }
+            })
+          }else if(ctx.callbackQuery.data == "battle_retreat"){
+            battleLeave(ctx);
+          }
+        }
+      })
+    }else{ctx.reply('Какой бой, если ты даже не рождён!');}
+  })
+  // Using context shortcut
+  ctx.answerCbQuery()
+})
+
+function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+class Item{
+  static table = _tables._itemTable;
+  static getItemByName(name,clb){
+    const mongoClient = new MongoClient(_url, _setting);
+    mongoClient.connect(function(err, client){
+      if(err) return console.log(err,'err');
+      const db = client.db(_DB);
+      db.collection(Item.table).findOne({n:name}, function(err, result) {
+        if (err) console.log(err,'err');
+        client.close();
+        clb(result);
+      });
+    });
   }
 }
 
 class Location {
-  static table = _tables._location;
+  static table = _tables._locationTable;
   static getLocationByUser(us, clb){
     const mongoClient = new MongoClient(_url, _setting);
     mongoClient.connect(function(err, client){
@@ -345,6 +466,24 @@ class dbWork {
       });
     });
     dbWork.creatTable(Battle.table)
+    dbWork.creatTable(Item.table, ()=>{
+      /*Load JSON*/
+      const ww = JSON.parse(fs.readFileSync('json/item.json', 'utf8'));
+      var ii = Object.keys(ww).map(key => {
+        return ww[key];
+      })
+      const mongoClient = new MongoClient(_url, _setting);
+      mongoClient.connect(function(err, client){
+        const db = client.db(_DB);
+        const collection = db.collection(Item.table);
+        collection.insertMany(ii, function(err, result){
+            if(err){ 
+                return console.log(err,'err');
+            }
+            client.close();
+        });
+      });
+    })
   }
 
   static creatTable(table, clb = undefined){
@@ -448,7 +587,11 @@ bot.command(['cultivate','cult','c'], (ctx) => {
 })
 
 bot.command(['baCancle','bac'], (ctx) => {
-  User.getUser(ctx.update.message.from, (user) => {
+  battleLeave(ctx);
+})
+
+function battleLeave(ctx){
+  User.getUser(ctx.from, (user) => {
     if(user != undefined){
       Battle.checkStartedBattle(user,(what)=>{
         if(what){
@@ -461,7 +604,7 @@ bot.command(['baCancle','bac'], (ctx) => {
       })
     }
   })
-})
+}
 
 bot.command(['clCancle','cl'], (ctx) => {
   User.getUser(ctx.update.message.from, (user) => {
@@ -561,13 +704,20 @@ bot.command(['immortals','im','imm'], (ctx) => {
   });
 })
 
+bot.command('t',(ctx)=>{ //------------------------------------------------------------------------TEST
+  User.getUser(ctx.from,(us)=>{
+    var user = Object.assign(new User, us);
+    user.addItem("crystal_low_quality",2);
+  })
+})
+
 bot.start((ctx) => {
   User.getUser(ctx.update.message.from, (user) => {
     if(user == undefined){
       User.insertUser(new User(ctx.update.message.from));
     }
   });
-  ctx.reply(`Это культиваторский бот. \n НАЧНИ СВОЮ КУЛЬТИВАЦИЮ!!! \n ${_helpCommands}`)
+  ctx.reply(`Вы появились в этом мире! \n ${_helpCommands}`)
 })
 bot.help((ctx) => ctx.reply(`${_helpCommands}`))
 
